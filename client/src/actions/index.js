54de0873e9968d16e7f7631322228160492688ab
Shashank Agarwal <agarwalshashank@pop-os.localdomain>
2020-06-25 18:37:26 +0530

    ssl changes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,17 +2,17 @@ import axios from "axios";
 import { FETCH_USER, ADMIN_LOGGED_IN } from "./types.js";
 
 export const fetchUser = () => async (dispatch) => {
-	const res = await axios.post("api/current_user");
-	dispatch({ type: FETCH_USER, payload: res.data });
+  const res = await axios.post("api/current_user");
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const checkAdmin = (e, adminPass) => async (dispatch) => {
-	e.preventDefault();
-	const res = await axios.post("api/check_admin", { password: adminPass });
-	if (res.data.success) {
-		dispatch({ type: ADMIN_LOGGED_IN, payload: res.data });
-	}
-	if (res.data.failure) {
-		dispatch({ type: ADMIN_LOGGED_IN, payload: res.data });
-	}
+  e.preventDefault();
+  const res = await axios.post("api/check_admin", { password: adminPass });
+  if (res.data.success) {
+    dispatch({ type: ADMIN_LOGGED_IN, payload: res.data });
+  }
+  if (res.data.failure) {
+    dispatch({ type: ADMIN_LOGGED_IN, payload: res.data });
+  }
 };
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
+const sslRedirect = require("heroku-ssl-redirect");
 require("./models/pendingUser.js");
 require("./models/user.js");
 require("./models/superAdmin.js");
@@ -20,10 +21,10 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(
-	cookieSession({
-		maxAge: 30 * 24 * 60 * 60 * 1000,
-		keys: [keys.cookieKey],
-	})
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
 );
 
 app.use(passport.initialize());
@@ -33,12 +34,13 @@ require("./routes/authRoutes.js")(app);
 require("./routes/membersRoutes.js")(app);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+  app.use(sslRedirect());
+  app.use(express.static("client/build"));
 
-	const path = require("path");
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	});
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
